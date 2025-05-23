415bb209322beae42d8b9be472c204875af3beb2
mykiael <106236307+mykiael@users.noreply.github.com>
2025-05-23 15:44:01 +0100

    Update lab_work_1.py

--- lab_work_1.py
+++ lab_work_1.py
@@ -7,19 +7,19 @@ for num in numbers:
     total += num
 
 print("The sum of the numbers is:", total)
-=======
-# Lab Work 1 - Weird Number Checker
+# =======
+# # Lab Work 1 - Weird Number Checker
 
-# Collect user input
-n = int(input("Enter an integer: "))
+# # Collect user input
+# n = int(input("Enter an integer: "))
 
-# Check the conditions
-if n % 2 != 0:
-    print("Weird")
-elif n % 2 == 0 and 2 <= n <= 5:
-    print("Not Weird")
-elif n % 2 == 0 and 6 <= n <= 20:
-    print("Weird")
-elif n % 2 == 0 and n > 20:
-    print("Not Weird")
->>>>>>> 58b033ad7d5aa6765c577abf71cf928fd34bdc7a
+# # Check the conditions
+# if n % 2 != 0:
+#     print("Weird")
+# elif n % 2 == 0 and 2 <= n <= 5:
+#     print("Not Weird")
+# elif n % 2 == 0 and 6 <= n <= 20:
+#     print("Weird")
+# elif n % 2 == 0 and n > 20:
+#     print("Not Weird")
+# >>>>>>> 58b033ad7d5aa6765c577abf71cf928fd34bdc7a
